35c3e35261fb8880a6e16984f7fe186ee39ea841
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-06-01 17:37:40 -0400

    removing drizzle

--- drizzle/0000_free_the_spike.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users_table` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`age` int NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`created_at` timestamp DEFAULT (now()),
-	CONSTRAINT `users_table_id` PRIMARY KEY(`id`),
-	CONSTRAINT `users_table_email_unique` UNIQUE(`email`)
-);
